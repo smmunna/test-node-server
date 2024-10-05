ab8d1c1c576020b71850205c6d9a1b61c6f993fe
smmunna <minhazulabedinmunna@gmail.com>
2024-10-05 20:40:24 +0600

    imgbb-uploader added

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/uploads',express.static('uploads'))
+app.use('/uploads', express.static('uploads'))
 
 // Route handlings;
 app.use('/api/v1/users', userRoutes)
